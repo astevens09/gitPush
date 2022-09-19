7d6315b3fb8665fa7ec0aac963c0b4bf5a6174fb
Anthony Stevens <aastevens1126@gmail.com>
2022-09-19 14:53:19 -0500

    peer to peer comms established

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ let TOKEN= "";
 let uid = String(Math.floor(Math.random()*10000));
 console.log(uid);
 
+//AgoraRTM client and channels
 let client;
 let channel;
 
+//WebRTC streams and connections
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -48,9 +50,25 @@ let init = async ()=>{
 }
 
 //Handle function for `MessageFromPeer` event
+//These message are handled relative to the computer
 let handleMessageFromPeer = async (message, MemberId) =>{
+    
     message = JSON.parse(message.text);
-    console.log('Message: ', message);
+
+    if(message.type == 'offer'){
+        await createAnswer(MemberId, message.offer);
+    }
+
+    if(message.type == 'answer'){
+        await addAnswer(message.answer);
+    }
+
+    if(message.type == 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate);
+        }
+    }
+    
 }
 
 
@@ -60,14 +78,22 @@ let handleUserJoined = async (MemberId)=>{
     createOffer(MemberId);
 }
 
-//Create offer function or other peer
-//This interface stores all the information between the local and remote peer
-//and it provides methods to connect to that peer
-let createOffer = async (MemberId)=>{
+//This function function creates the answer from remote peer
+let createPeerConnection = async (MemberId) =>{
     peerConnection = new RTCPeerConnection(servers);
 
     remoteStream = new MediaStream;
     document.querySelector('#stream2').srcObject = remoteStream;
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({ //getUserMedia return an object
+            video:true,
+            audio:false
+        });
+    
+        //Use the stream
+        document.querySelector('#stream1').srcObject = localStream;
+    }
 
 
     //Get localStream audio 
@@ -89,17 +115,42 @@ let createOffer = async (MemberId)=>{
             console.log('New ice candidate: ', event.candidate)
         }
     }
+}
+
+//Create offer function or other peer
+//This interface stores all the information between the local and remote peer
+//and it provides methods to connect to that peer
+let createOffer = async (MemberId)=>{
+    await createPeerConnection(MemberId);
 
     //Create and set session description
-    let sessionDescription = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(sessionDescription);
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
 
     //AgoraRTM client send message to peer
-    client.sendMessageToPeer({text: JSON.stringify({"type": 'offer', "offer": sessionDescription})}, MemberId);
+    client.sendMessageToPeer({text: JSON.stringify({"type": 'offer', "offer": offer})}, MemberId);
 
 }
 
-// function init
+//Create answer function back to initial peer
+let createAnswer = async(MemberId, offer) =>{
+    await createPeerConnection(MemberId);
 
+    await peerConnection.setRemoteDescription(offer);
+
+    let answer = await peerConnection.createAnswer();
+    peerConnection.setLocalDescription(answer);
+
+    //AgoraRTM client send message to peer
+    client.sendMessageToPeer({text: JSON.stringify({"type": 'answer', "answer": answer})}, MemberId);
+}
+
+
+//Add answer to remote description
+let addAnswer = async (answer) =>{
+    if(!peerConnection.currentRemoteDescription){
+      await peerConnection.setRemoteDescription(answer);
+    }
+}
 //Call init function
 init();
